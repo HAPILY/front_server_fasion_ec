571a80a45ec34b55487a48d1c2fd53074f9a93aa
shogo-takao <taka.sho2508@gmail.com>
2019-10-01 23:23:14 +0900

    not use typescript

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,23 @@
 module.exports = {
   root: true,
   env: {
-      browser: true,
-      node: true
+    browser: true,
+    node: true
+  },
+  parserOptions: {
+    parser: 'babel-eslint'
   },
   extends: [
-      '@nuxtjs/eslint-config-typescript',
-      'plugin:prettier/recommended',
-      'prettier',
-      'prettier/vue'
+    '@nuxtjs',
+    'prettier',
+    'prettier/vue',
+    'plugin:prettier/recommended',
+    'plugin:nuxt/recommended'
   ],
   plugins: [
-      'vue'
+    'prettier'
   ],
+  // add your custom rules here
   rules: {
-      'vue/html-closing-bracket-newline': 'off',
-      'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-      'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-      'space-before-function-paren': 0
   }
 }
--- pages/index.vue
+++ pages/index.vue
@@ -1,42 +1,68 @@
 <template>
-  <section>
-    <h1 class="header">Nuxt TypeScript Starter</h1>
-    <div class="cards">
-      <Card
-        v-for="person in people"
-        :key="person.id"
-        :person="person"
-      ></Card>
+  <div class="container">
+    <div>
+      <logo />
+      <h1 class="title">
+        test
+      </h1>
+      <h2 class="subtitle">
+        My delightful Nuxt.js project
+      </h2>
+      <div class="links">
+        <a href="https://nuxtjs.org/" target="_blank" class="button--green">
+          Documentation
+        </a>
+        <a
+          href="https://github.com/nuxt/nuxt.js"
+          target="_blank"
+          class="button--grey"
+        >
+          GitHub
+        </a>
+      </div>
     </div>
-  </section>
+  </div>
 </template>
 
-<script lang="ts">
-import {
-  Component,
-  Vue
-} from "nuxt-property-decorator"
-import { State } from "vuex-class"
-import { Person } from "~/types";
-import Card from "~/components/Card.vue"
+<script>
+import Logo from '~/components/Logo.vue'
 
-@Component({
+export default {
   components: {
-    Card
+    Logo
   }
-})
-export default class extends Vue {
-  @State people!: Person
 }
 </script>
 
-<style scoped>
-.header {
-  font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
 }
 
-.cards {
-  display: flex;
-  flex-wrap: wrap;
+.title {
+  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
 }
 </style>
