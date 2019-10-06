23539e4a33d0eb6ebbf661a47c48fff23665d18e
shogo-takao <taka.sho2508@gmail.com>
2019-10-06 18:37:33 +0900

    about page

--- pages/about/index.vue
+++ pages/about/index.vue
@@ -1,6 +1,17 @@
 <template>
   <div class="about">
-    <div :style="{ background: '#fff', padding: '24px', minHeight: '280px' }">About</div>
+    <h1>Concept</h1>
+    <div class="about-visual">
+      <img alt="example" src="~/assets/images/demo.png" slot="cover" />
+    </div>
+    <div class="about-main">
+      <h2>変わらぬ想いでよろこばれる美味しさを</h2>
+      <div class="about-main-concept">
+        中尾清月堂は、お客様によろこばれる美味しさを大切にしています。
+        <br />創業当時から長年愛されるお菓子から、時代にあった新しいお菓子まで、
+        <br />そのひとつひとつが今も職人の手で、まごころを込めてつくられています。
+      </div>
+    </div>
     <Breadcrumbs :currentPage="{ name: 'About' }" />
   </div>
 </template>
@@ -14,3 +25,45 @@ export default {
   }
 };
 </script>
+
+<style lang="scss" scoped>
+.about {
+  height: 100%;
+  position: relative;
+  background: color(white, base);
+  padding: 24px;
+  padding-bottom: 8px;
+  min-height: 280px;
+
+  > h1 {
+    text-align: center;
+    font-family: Garton;
+    font-size: 28px;
+    margin-bottom: 10px;
+  }
+
+  &-visual {
+    margin-bottom: 30px;
+    text-align: center;
+
+    > img {
+      @include media(sm) {
+        width: 100%;
+      }
+    }
+  }
+
+  &-main {
+    text-align: center;
+    > h2 {
+      font-size: 22px;
+      font-weight: bold;
+      margin-bottom: 20px;
+    }
+
+    &-concept {
+      line-height: 1.4;
+    }
+  }
+}
+</style>
