37efb9e46b733bdd71f0a3f68af2e4d85b9c6119
shogo-takao <taka.sho2508@gmail.com>
2019-10-10 13:35:40 +0900

    change image path

--- agreed/api/item/normal.js
+++ agreed/api/item/normal.js
@@ -14,12 +14,12 @@ module.exports = {
           content: "オリジナル２個のギフトセットです。<br><br>バターの衝撃。香りを堪能できる王道<br>(小豆あん+バター)<br><br>新商品ホットドラバターは、その名の通り、小豆あんに加えバターを挟んだどら焼きです。<br>レンジで温めることで、小豆あんがトロトロ、皮はしっとりふわふわの食感になり、バターがジュワッと染み込みます。<br><br>ホットドラバターオリジナルは、小豆あんとバターの王道の組み合わせ!<br>バターは発酵バターと香りを際立たせるマーガリンを配合。<br>レンジから出した瞬間の香りが食欲をそそります。<br>温め時間: 600W 25~ 35秒/500W 30~40秒<br><br>この冬は、ぜひホッカホカのどら焼きで、心も体も温まってください。",
           images: [
             {
-              src: "https://www.gstatic.com/webp/gallery/4.sm.jpg",
-              webp: "https://www.gstatic.com/webp/gallery/4.sm.webp"
+              src: "https://www.gstatic.com/webp/gallery/1.sm.jpg",
+              webp: "https://www.gstatic.com/webp/gallery/1.sm.webp"
             },
             {
-              src: "https://www.gstatic.com/webp/gallery/4.sm.jpg",
-              webp: "https://www.gstatic.com/webp/gallery/4.sm.webp"
+              src: "https://www.gstatic.com/webp/gallery/2.sm.jpg",
+              webp: "https://www.gstatic.com/webp/gallery/2.sm.webp"
             },
             {
               src: "https://www.gstatic.com/webp/gallery/4.sm.jpg",
@@ -34,12 +34,12 @@ module.exports = {
           content: "オリジナル２個のギフトセットです。<br><br>バターの衝撃。香りを堪能できる王道<br>(小豆あん+バター)<br><br>新商品ホットドラバターは、その名の通り、小豆あんに加えバターを挟んだどら焼きです。<br>レンジで温めることで、小豆あんがトロトロ、皮はしっとりふわふわの食感になり、バターがジュワッと染み込みます。<br><br>ホットドラバターオリジナルは、小豆あんとバターの王道の組み合わせ!<br>バターは発酵バターと香りを際立たせるマーガリンを配合。<br>レンジから出した瞬間の香りが食欲をそそります。<br>温め時間: 600W 25~ 35秒/500W 30~40秒<br><br>この冬は、ぜひホッカホカのどら焼きで、心も体も温まってください。",
           images: [
             {
-              src: "https://www.gstatic.com/webp/gallery/4.sm.jpg",
-              webp: "https://www.gstatic.com/webp/gallery/4.sm.webp"
+              src: "https://www.gstatic.com/webp/gallery/5.sm.jpg",
+              webp: "https://www.gstatic.com/webp/gallery/5.sm.webp"
             },
             {
-              src: "https://www.gstatic.com/webp/gallery/4.sm.jpg",
-              webp: "https://www.gstatic.com/webp/gallery/4.sm.webp"
+              src: "https://www.gstatic.com/webp/gallery/3.sm.jpg",
+              webp: "https://www.gstatic.com/webp/gallery/3.sm.webp"
             }
           ]
         },
@@ -50,24 +50,24 @@ module.exports = {
           content: "オリジナル２個のギフトセットです。<br><br>バターの衝撃。香りを堪能できる王道<br>(小豆あん+バター)<br><br>新商品ホットドラバターは、その名の通り、小豆あんに加えバターを挟んだどら焼きです。<br>レンジで温めることで、小豆あんがトロトロ、皮はしっとりふわふわの食感になり、バターがジュワッと染み込みます。<br><br>ホットドラバターオリジナルは、小豆あんとバターの王道の組み合わせ!<br>バターは発酵バターと香りを際立たせるマーガリンを配合。<br>レンジから出した瞬間の香りが食欲をそそります。<br>温め時間: 600W 25~ 35秒/500W 30~40秒<br><br>この冬は、ぜひホッカホカのどら焼きで、心も体も温まってください。",
           images: [
             {
-              src: "https://www.gstatic.com/webp/gallery/4.sm.jpg",
-              webp: "https://www.gstatic.com/webp/gallery/4.sm.webp"
+              src: "https://www.gstatic.com/webp/gallery/2.sm.jpg",
+              webp: "https://www.gstatic.com/webp/gallery/2.sm.webp"
             },
             {
               src: "https://www.gstatic.com/webp/gallery/4.sm.jpg",
               webp: "https://www.gstatic.com/webp/gallery/4.sm.webp"
             },
             {
-              src: "https://www.gstatic.com/webp/gallery/4.sm.jpg",
-              webp: "https://www.gstatic.com/webp/gallery/4.sm.webp"
+              src: "https://www.gstatic.com/webp/gallery/5.sm.jpg",
+              webp: "https://www.gstatic.com/webp/gallery/5.sm.webp"
             },
             {
               src: "https://www.gstatic.com/webp/gallery/4.sm.jpg",
               webp: "https://www.gstatic.com/webp/gallery/4.sm.webp"
             },
             {
-              src: "https://www.gstatic.com/webp/gallery/4.sm.jpg",
-              webp: "https://www.gstatic.com/webp/gallery/4.sm.webp"
+              src: "https://www.gstatic.com/webp/gallery/1.sm.jpg",
+              webp: "https://www.gstatic.com/webp/gallery/1.sm.webp"
             }
           ]
         }
--- agreed/api/item/pickup.js
+++ agreed/api/item/pickup.js
@@ -58,16 +58,16 @@ module.exports = {
           content: "オリジナル２個のギフトセットです。<br><br>バターの衝撃。香りを堪能できる王道<br>(小豆あん+バター)<br><br>新商品ホットドラバターは、その名の通り、小豆あんに加えバターを挟んだどら焼きです。<br>レンジで温めることで、小豆あんがトロトロ、皮はしっとりふわふわの食感になり、バターがジュワッと染み込みます。<br><br>ホットドラバターオリジナルは、小豆あんとバターの王道の組み合わせ!<br>バターは発酵バターと香りを際立たせるマーガリンを配合。<br>レンジから出した瞬間の香りが食欲をそそります。<br>温め時間: 600W 25~ 35秒/500W 30~40秒<br><br>この冬は、ぜひホッカホカのどら焼きで、心も体も温まってください。",
           images: [
             {
-              src: "https://www.gstatic.com/webp/gallery/4.sm.jpg",
-              webp: "https://www.gstatic.com/webp/gallery/4.sm.webp"
+              src: "https://www.gstatic.com/webp/gallery/1.sm.jpg",
+              webp: "https://www.gstatic.com/webp/gallery/1.sm.webp"
             },
             {
               src: "https://www.gstatic.com/webp/gallery/4.sm.jpg",
               webp: "https://www.gstatic.com/webp/gallery/4.sm.webp"
             },
             {
-              src: "https://www.gstatic.com/webp/gallery/4.sm.jpg",
-              webp: "https://www.gstatic.com/webp/gallery/4.sm.webp"
+              src: "https://www.gstatic.com/webp/gallery/2.sm.jpg",
+              webp: "https://www.gstatic.com/webp/gallery/2.sm.webp"
             }
           ]
         },
@@ -78,16 +78,16 @@ module.exports = {
           content: "オリジナル２個のギフトセットです。<br><br>バターの衝撃。香りを堪能できる王道<br>(小豆あん+バター)<br><br>新商品ホットドラバターは、その名の通り、小豆あんに加えバターを挟んだどら焼きです。<br>レンジで温めることで、小豆あんがトロトロ、皮はしっとりふわふわの食感になり、バターがジュワッと染み込みます。<br><br>ホットドラバターオリジナルは、小豆あんとバターの王道の組み合わせ!<br>バターは発酵バターと香りを際立たせるマーガリンを配合。<br>レンジから出した瞬間の香りが食欲をそそります。<br>温め時間: 600W 25~ 35秒/500W 30~40秒<br><br>この冬は、ぜひホッカホカのどら焼きで、心も体も温まってください。",
           images: [
             {
-              src: "https://www.gstatic.com/webp/gallery/4.sm.jpg",
-              webp: "https://www.gstatic.com/webp/gallery/4.sm.webp"
+              src: "https://www.gstatic.com/webp/gallery/3.sm.jpg",
+              webp: "https://www.gstatic.com/webp/gallery/3.sm.webp"
             },
             {
               src: "https://www.gstatic.com/webp/gallery/4.sm.jpg",
               webp: "https://www.gstatic.com/webp/gallery/4.sm.webp"
             },
             {
-              src: "https://www.gstatic.com/webp/gallery/4.sm.jpg",
-              webp: "https://www.gstatic.com/webp/gallery/4.sm.webp"
+              src: "https://www.gstatic.com/webp/gallery/5.sm.jpg",
+              webp: "https://www.gstatic.com/webp/gallery/5.sm.webp"
             }
           ]
         },
@@ -102,12 +102,12 @@ module.exports = {
               webp: "https://www.gstatic.com/webp/gallery/4.sm.webp"
             },
             {
-              src: "https://www.gstatic.com/webp/gallery/4.sm.jpg",
-              webp: "https://www.gstatic.com/webp/gallery/4.sm.webp"
+              src: "https://www.gstatic.com/webp/gallery/5.sm.jpg",
+              webp: "https://www.gstatic.com/webp/gallery/5.sm.webp"
             },
             {
-              src: "https://www.gstatic.com/webp/gallery/4.sm.jpg",
-              webp: "https://www.gstatic.com/webp/gallery/4.sm.webp"
+              src: "https://www.gstatic.com/webp/gallery/2.sm.jpg",
+              webp: "https://www.gstatic.com/webp/gallery/2.sm.webp"
             }
           ]
         },
@@ -122,12 +122,12 @@ module.exports = {
               webp: "https://www.gstatic.com/webp/gallery/4.sm.webp"
             },
             {
-              src: "https://www.gstatic.com/webp/gallery/4.sm.jpg",
-              webp: "https://www.gstatic.com/webp/gallery/4.sm.webp"
+              src: "https://www.gstatic.com/webp/gallery/1.sm.jpg",
+              webp: "https://www.gstatic.com/webp/gallery/1.sm.webp"
             },
             {
-              src: "https://www.gstatic.com/webp/gallery/4.sm.jpg",
-              webp: "https://www.gstatic.com/webp/gallery/4.sm.webp"
+              src: "https://www.gstatic.com/webp/gallery/3.sm.jpg",
+              webp: "https://www.gstatic.com/webp/gallery/3.sm.webp"
             }
           ]
         },
@@ -138,8 +138,8 @@ module.exports = {
           content: "オリジナル２個のギフトセットです。<br><br>バターの衝撃。香りを堪能できる王道<br>(小豆あん+バター)<br><br>新商品ホットドラバターは、その名の通り、小豆あんに加えバターを挟んだどら焼きです。<br>レンジで温めることで、小豆あんがトロトロ、皮はしっとりふわふわの食感になり、バターがジュワッと染み込みます。<br><br>ホットドラバターオリジナルは、小豆あんとバターの王道の組み合わせ!<br>バターは発酵バターと香りを際立たせるマーガリンを配合。<br>レンジから出した瞬間の香りが食欲をそそります。<br>温め時間: 600W 25~ 35秒/500W 30~40秒<br><br>この冬は、ぜひホッカホカのどら焼きで、心も体も温まってください。",
           images: [
             {
-              src: "https://www.gstatic.com/webp/gallery/4.sm.jpg",
-              webp: "https://www.gstatic.com/webp/gallery/4.sm.webp"
+              src: "https://www.gstatic.com/webp/gallery/5.sm.jpg",
+              webp: "https://www.gstatic.com/webp/gallery/5.sm.webp"
             },
             {
               src: "https://www.gstatic.com/webp/gallery/4.sm.jpg",
@@ -158,16 +158,16 @@ module.exports = {
           content: "オリジナル２個のギフトセットです。<br><br>バターの衝撃。香りを堪能できる王道<br>(小豆あん+バター)<br><br>新商品ホットドラバターは、その名の通り、小豆あんに加えバターを挟んだどら焼きです。<br>レンジで温めることで、小豆あんがトロトロ、皮はしっとりふわふわの食感になり、バターがジュワッと染み込みます。<br><br>ホットドラバターオリジナルは、小豆あんとバターの王道の組み合わせ!<br>バターは発酵バターと香りを際立たせるマーガリンを配合。<br>レンジから出した瞬間の香りが食欲をそそります。<br>温め時間: 600W 25~ 35秒/500W 30~40秒<br><br>この冬は、ぜひホッカホカのどら焼きで、心も体も温まってください。",
           images: [
             {
-              src: "https://www.gstatic.com/webp/gallery/4.sm.jpg",
-              webp: "https://www.gstatic.com/webp/gallery/4.sm.webp"
+              src: "https://www.gstatic.com/webp/gallery/1.sm.jpg",
+              webp: "https://www.gstatic.com/webp/gallery/1.sm.webp"
             },
             {
-              src: "https://www.gstatic.com/webp/gallery/4.sm.jpg",
-              webp: "https://www.gstatic.com/webp/gallery/4.sm.webp"
+              src: "https://www.gstatic.com/webp/gallery/2.sm.jpg",
+              webp: "https://www.gstatic.com/webp/gallery/2.sm.webp"
             },
             {
-              src: "https://www.gstatic.com/webp/gallery/4.sm.jpg",
-              webp: "https://www.gstatic.com/webp/gallery/4.sm.webp"
+              src: "https://www.gstatic.com/webp/gallery/3.sm.jpg",
+              webp: "https://www.gstatic.com/webp/gallery/3.sm.webp"
             }
           ]
         },
@@ -178,12 +178,12 @@ module.exports = {
           content: "オリジナル２個のギフトセットです。<br><br>バターの衝撃。香りを堪能できる王道<br>(小豆あん+バター)<br><br>新商品ホットドラバターは、その名の通り、小豆あんに加えバターを挟んだどら焼きです。<br>レンジで温めることで、小豆あんがトロトロ、皮はしっとりふわふわの食感になり、バターがジュワッと染み込みます。<br><br>ホットドラバターオリジナルは、小豆あんとバターの王道の組み合わせ!<br>バターは発酵バターと香りを際立たせるマーガリンを配合。<br>レンジから出した瞬間の香りが食欲をそそります。<br>温め時間: 600W 25~ 35秒/500W 30~40秒<br><br>この冬は、ぜひホッカホカのどら焼きで、心も体も温まってください。",
           images: [
             {
-              src: "https://www.gstatic.com/webp/gallery/4.sm.jpg",
-              webp: "https://www.gstatic.com/webp/gallery/4.sm.webp"
+              src: "https://www.gstatic.com/webp/gallery/5.sm.jpg",
+              webp: "https://www.gstatic.com/webp/gallery/5.sm.webp"
             },
             {
-              src: "https://www.gstatic.com/webp/gallery/4.sm.jpg",
-              webp: "https://www.gstatic.com/webp/gallery/4.sm.webp"
+              src: "https://www.gstatic.com/webp/gallery/1.sm.jpg",
+              webp: "https://www.gstatic.com/webp/gallery/1.sm.webp"
             },
             {
               src: "https://www.gstatic.com/webp/gallery/4.sm.jpg",
@@ -198,16 +198,16 @@ module.exports = {
           content: "オリジナル２個のギフトセットです。<br><br>バターの衝撃。香りを堪能できる王道<br>(小豆あん+バター)<br><br>新商品ホットドラバターは、その名の通り、小豆あんに加えバターを挟んだどら焼きです。<br>レンジで温めることで、小豆あんがトロトロ、皮はしっとりふわふわの食感になり、バターがジュワッと染み込みます。<br><br>ホットドラバターオリジナルは、小豆あんとバターの王道の組み合わせ!<br>バターは発酵バターと香りを際立たせるマーガリンを配合。<br>レンジから出した瞬間の香りが食欲をそそります。<br>温め時間: 600W 25~ 35秒/500W 30~40秒<br><br>この冬は、ぜひホッカホカのどら焼きで、心も体も温まってください。",
           images: [
             {
-              src: "https://www.gstatic.com/webp/gallery/4.sm.jpg",
-              webp: "https://www.gstatic.com/webp/gallery/4.sm.webp"
+              src: "https://www.gstatic.com/webp/gallery/2.sm.jpg",
+              webp: "https://www.gstatic.com/webp/gallery/2.sm.webp"
             },
             {
               src: "https://www.gstatic.com/webp/gallery/4.sm.jpg",
               webp: "https://www.gstatic.com/webp/gallery/4.sm.webp"
             },
             {
-              src: "https://www.gstatic.com/webp/gallery/4.sm.jpg",
-              webp: "https://www.gstatic.com/webp/gallery/4.sm.webp"
+              src: "https://www.gstatic.com/webp/gallery/2.sm.jpg",
+              webp: "https://www.gstatic.com/webp/gallery/2.sm.webp"
             }
           ]
         }
