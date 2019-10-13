4984f56ef050d5179ebe61a452bbdd734a1130ed
shogo-takao <taka.sho2508@gmail.com>
2019-10-13 21:39:03 +0900

    login page

---
+++ pages/login/index.vue
@@ -0,0 +1,119 @@
+<template>
+  <div class="login">
+    <h1 class="login-title">
+      Login
+    </h1>
+    <a-form
+      class="login-form"
+      :form="form"
+      @submit="handleSubmit"
+    >
+      <a-form-item
+        class="login-mail"
+        label="メールアドレス"
+      >
+        <a-input
+          v-decorator="[
+            'email',
+            {
+              rules: [{
+                type: 'email', message: 'メールアドレスの形式が間違っています',
+              }, {
+                required: true, message: '必須項目です',
+              }]
+            }
+          ]"
+        />
+      </a-form-item>
+      <a-form-item
+        class="login-pass"
+        label="パスワード"
+      >
+        <a-input
+          v-decorator="[
+            'password',
+            {
+              rules: [{
+                required: true, message: '必須項目です',
+              }]
+            }
+          ]"
+          type="password"
+          :len="4"
+        />
+      </a-form-item>
+      <a-form-item
+        class="login-submit"
+      >
+        <div class="login-create">
+          <nuxt-link to="/login/create">
+            アカウント作成
+          </nuxt-link>
+        </div>
+        <a-button
+          type="primary"
+          html-type="submit"
+        >
+          ログイン
+        </a-button>
+      </a-form-item>
+    </a-form>
+  </div>
+</template>
+
+<script>
+export default {
+  beforeCreate () {
+    this.form = this.$form.createForm(this);
+  },
+  methods: {
+    handleSubmit  (e) {
+      e.preventDefault();
+      this.form.validateFieldsAndScroll((err, values) => {
+        if (!err) {
+          console.log('Received values of form: ', values);
+        }
+      });
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.login {
+  height: 100vh;
+  position: relative;
+  background: color(white, base);
+  padding: 24px;
+  padding-bottom: 8px;
+  min-height: 280px;
+
+  &-title {
+    font-size: 32px;
+    font-weight: bold;
+    text-align: center;
+    font-family: Garton;
+    margin-bottom: 28px;
+  }
+
+  &-form {
+    width: 70%;
+    margin: 0 auto;
+  }
+
+  &-mail,
+  &-pass {
+    margin-bottom: 10px;
+  }
+
+  &-create {
+    display: inline-block;
+    font-size: 14px;
+    margin-right:20px;
+  }
+
+  &-submit {
+    text-align: right;
+  }
+}
+</style>
