dc11d42c65274e94de5b7440bb5f4e616a47477c
shogo-takao <taka.sho2508@gmail.com>
2019-10-21 21:59:06 +0900

    loading skeleton

---
+++ components/skeleton/SkeletonCard.vue
@@ -0,0 +1,19 @@
+<template>
+  <a-skeleton
+    active
+    :paragraph="{rows: 2}"
+  />
+</template>
+
+<script>
+export default {
+  name: "SkeletonCard"
+}
+</script>
+
+<style lang="scss" scoped>
+/deep/ .ant-skeleton-title {
+  width: 100% !important;
+  height: 300px;
+}
+</style>
