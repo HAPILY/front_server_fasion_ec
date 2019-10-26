eaf04a5eb764cb8de490ebf5e64b72b70b5818ae
THitokuse <hasegawa.taichiro@gmail.com>
2019-10-26 16:10:30 +0900

    create table admin item-table in items_index.vue

---
+++ const/AdminHeader.js
@@ -0,0 +1,20 @@
+export default {
+  items: [
+    {
+      title: '商品管理',
+      to: '/admin/items'
+    },
+    {
+      title: 'ユーザー管理',
+      to: '/admin/users'
+    },
+    {
+      title: 'ニュース管理',
+      to: '/admin/news'
+    },
+    {
+      title: '企業情報管理',
+      to: '/admin/about'
+    }
+  ]
+}
---
+++ pages/admin/items/index.vue
@@ -0,0 +1,74 @@
+<template>
+  <a-table
+    :columns="columns"
+    :dataSource="items"
+    :loading="loading"
+  >
+    <span
+      slot="stock"
+      slot-scope="record"
+    >
+      <input
+        type="number"
+        :value="record.stock"
+        min="0"
+        max="1000"
+      >
+    </span>
+    <span slot="action">
+      <a
+        href="#"
+        class="ant-dropdown-link"
+      >詳細<a-icon type="down" /> </a>
+      <a-button type="danger">削除</a-button>
+    </span>
+  </a-table>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      items: [
+        { id: 1, name: "Chandler Bing", stock: 2, content: 'これはいい物です。'},
+        { id: 2, name: "Ross Geller", stock: 1, content: 'これはいい物です。'},
+        { id: 3, name: "Rachel Green", stock: 2, content: 'これはいい物です。'},
+        { id: 4, name: "Monica Geller", stock: 2, content: 'これはいい物です。'},
+        { id: 5, name: "Joey Tribbiani", stock: 2, content: 'これはいい物です。'},
+        { id: 6, name: "Phoebe Buffay", stock: 2, content: 'これはいい物です。'}
+      ],
+      columns: [
+        {
+          title: 'ID',
+          dataIndex: 'id',
+          sorter: true
+        },
+        {
+          title: 'Name',
+          dataIndex: 'name'
+        },
+        {
+          title: 'content',
+          dataIndex: 'content'
+        },
+        {
+          title: 'stock',
+          key: 'stock',
+          scopedSlots: { customRender: 'stock' }
+        },
+        {
+          title: 'Action',
+          key: 'operation',
+          fixed: 'right',
+          width: 100,
+          scopedSlots: { customRender: 'action' },
+        }
+      ],
+      pagination: {},
+      loading: false
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
